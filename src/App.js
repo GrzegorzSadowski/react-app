2ff4d0ff5fd88af8afebf731bcb9027baa77e498
Grzegorz Sadowski <34543404+GrzegorzSadowski@users.noreply.github.com>
2022-11-29 08:04:09 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PageNotFound from "./components/NotFoundPage/NotFoundPage";
+
+ function App() {
+    return (
+        <>
+            <PageNotFound />
+        </>
+    );
+}
+export default App;
